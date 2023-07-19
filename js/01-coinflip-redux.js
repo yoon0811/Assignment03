73ba4cc2e978bd91802b0a4248e24d17798c65d8
Hyeyoon Jeong <117424725+yoon0811@users.noreply.github.com>
2023-07-19 17:15:41 +0900

    Update 01-coinflip-redux.js
    check string or number
    random cont 0 and 1

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -1,5 +1,21 @@
-let a = 5;
-let i;
-for (i = 0; i < 3; i++) {
-     console.log(a);
+
+let coinFlip;
+let check = /^[0-9]+$/; 
+
+
+flipCount = Number(prompt('How many times you want to flip?'));
+
+if (!check.test(flipCount)) {
+     console.log('please enter a number.')
+} else{
+     for(i = 0; i < flipCount; i++){
+          console.log(flipCount);
+          coinFlip = Math.round(Math.random());
+          if (coinFlip === 1){
+               console.log('Tails');
+          } else if(coinFlip === 0) {
+               console.log('Head')
+          }
+     }
+     
 }
